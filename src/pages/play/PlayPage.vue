15fd6ebc324e0563a67e7de137e5983e398ac261
root <root@localhost.localdomain>
2026-04-14 22:32:54 +0800

    fix(play): decouple history list bootstrap from forced detail stage

--- src/pages/play/PlayPage.vue
+++ src/pages/play/PlayPage.vue
@@ -4287,6 +4287,7 @@ export default {
       const forceDetailOnlyByStage = stageState.done && !stageState.detailDone;
       let detailRequested = !!forceDetailOnlyByStage;
       let hasHistoryContext = false;
+      let historyListPlayed = false;
 
       const buildTargetKey = (target) => {
         const siteItem = target && target.siteItem ? target.siteItem : null;
@@ -4360,7 +4361,10 @@ export default {
         const listTarget = forceDetailOnlyByStage ? null : await resolveHistoryTarget(false);
         if (listTarget) hasHistoryContext = true;
         if (listTarget && listTarget.fromHistoryPlayFlag) {
-          if (await tryPlayTarget(listTarget)) return true;
+          if (await tryPlayTarget(listTarget)) {
+            historyListPlayed = true;
+            return true;
+          }
         } else if (listTarget && listTarget.fromHistoryDetail) {
           detailRequested = true;
           if (await tryPlayTarget(listTarget)) return true;
@@ -4404,7 +4408,7 @@ export default {
 
         return false;
       } finally {
-        const detailDone = !hasHistoryContext || !!detailRequested;
+        const detailDone = historyListPlayed || !hasHistoryContext || !!detailRequested;
         this.historySmartBootstrapStageDone = {
           ...this.historySmartBootstrapStageDone,
           [stageKey]: { done: true, detailDone },
